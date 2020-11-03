4100768a9f5e0b0d041d95948321b4f72d6eb088
Joshua Ji <joshuanianji@users.noreply.github.com>
2020-11-02 23:12:24 -0700

    add tokio, update Client::builder()

--- src/main.rs
+++ src/main.rs
@@ -49,7 +49,7 @@ impl EventHandler for Handler {
 async fn main() {
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
-    let mut client = Client::new(&token)
+    let mut client = Client::builder(&token)
         .event_handler(Handler)
         .await
         .expect("Err creating client");
