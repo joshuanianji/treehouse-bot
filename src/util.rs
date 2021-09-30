0d5b6248f6d8c5d752e8cb25802ce445e2455c20
Joshua Ji <joshua.ji@jiwhiz.com>
2021-09-29 21:10:57 -0600

    Delete all the files - gonna repurpose the bot

--- src/util.rs
+++
@@ -1,8 +0,0 @@
-use serenity;
-
-/// Checks that a message successfully sent; if not, then logs why to stdout.
-pub fn check_msg(result: serenity::Result<serenity::model::channel::Message>) {
-    if let Err(why) = result {
-        println!("Error sending message: {:?}", why);
-    }
-}
