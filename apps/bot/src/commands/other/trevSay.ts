8f456ce0039416ff4102b0123ecc18e2c8f2eb7f
Joshua Ji <joshua.ji@jiwhiz.com>
2021-12-28 19:17:56 -0700

    fix(bot): :goal_net: Add better error handling, use SERVER_HOST

--- apps/bot/src/commands/other/trevSay.ts
+++ apps/bot/src/commands/other/trevSay.ts
@@ -1,6 +1,7 @@
 import { Command } from '../../interfaces';
 import { TrevResponse } from 'custom-types'
 import axios from 'axios';
+import { MessageEmbed } from 'discord.js'
 
 export const command: Command = {
     // Note aliases are optional
@@ -10,19 +11,27 @@ export const command: Command = {
     name: "trevsay",
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     run: async (client, msg, args) => {
-        const text = args.join(' ');
-        const res = await axios.get<TrevResponse>(`http://localhost:3001/trev?text=${encodeURI(text)}`)
-        const data = res.data;
+        try {
+            const text = args.join(' ');
+            const res = await axios.get<TrevResponse>(`${process.env.SERVER_HOST}/trev?text=${encodeURI(text)}`)
+            const data = res.data;
 
-        console.log('Got image, dimensions are:', data.width, data.height);
+            console.log('Got image, dimensions are:', data.width, data.height);
 
-        // convert data to bugger
-        const b64image = data.data.split(',')[1];
-        const buf = Buffer.from(b64image, 'base64');
+            // convert data to bugger
+            const b64image = data.data.split(',')[1];
+            const buf = Buffer.from(b64image, 'base64');
 
-        return msg.channel.send({
-            content: 'hi',
-            files: [buf]
-        })
+            return msg.channel.send({
+                content: 'hi',
+                files: [buf]
+            })
+        } catch (e) {
+            console.error(e);
+            const embed = new MessageEmbed()
+                .setTitle('Something went wrong!')
+                .addField('Error', JSON.stringify(e), false)
+            return msg.channel.send({ embeds: [embed] });
+        }
     }
 };
