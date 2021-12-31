42e05737f0082da3e6cc762211e31579f41d8363
Joshua Ji <joshua.ji@jiwhiz.com>
2021-12-31 13:09:45 -0700

    feat(server): Add SIGTERM signal
    Not sure if this does anything significant lol

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -18,7 +18,14 @@ app.use(responseTime());
 
 app.use('/', getRoutes())
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Application started on port ${PORT}!`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    console.log('HTTP server closed')
+  })
+})
