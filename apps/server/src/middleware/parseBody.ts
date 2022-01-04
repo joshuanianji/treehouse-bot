9083f3291cdddcab85681b786f205a5818a03b5d
Joshua Ji <joshua.ji@jiwhiz.com>
2022-01-04 10:55:43 -0700

    feat(server): :sparkles: Add typesafe `parseBody` middleware with `io-ts`
    With tests!

---
+++ apps/server/src/middleware/parseBody.ts
@@ -0,0 +1,25 @@
+import { RequestHandler } from 'express';
+import { pipe } from 'fp-ts/lib/function';
+import { fold } from 'fp-ts/lib/Either';
+import * as i from 'io-ts'
+import { ParamsDictionary } from 'express-serve-static-core'
+import * as D from 'io-ts/lib/Decoder';
+import { formatValidationErrors } from 'io-ts-reporters'
+
+
+// https://blog.jiayihu.net/how-to-validate-express-requests-using-io-ts/
+
+export const parseBody = <T>(decoder: i.Decoder<unknown, T>): RequestHandler<ParamsDictionary, any, T> => (
+    req,
+    res,
+    next,
+) => {
+    return pipe(
+        decoder.decode(req.body),
+        fold(
+            errors => res.status(400).send({ code: 'BadArgument', status: 'error', error: formatValidationErrors(errors) }),
+            () => next(),
+        ),
+    );
+};
+
