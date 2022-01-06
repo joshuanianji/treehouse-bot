a2beddafa70c975f0df774a652c9d3715fa61edd
Joshua Ji <joshua.ji@jiwhiz.com>
2022-01-06 15:57:42 -0700

    Make `,nft list` command look better

--- apps/bot/src/commands/nft/listNFT.ts
+++ apps/bot/src/commands/nft/listNFT.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import { NFT } from 'custom-types';
+import { NFT, NFTType } from 'custom-types';
 import { Either, right, left } from 'fp-ts/lib/Either';
 import { fold } from 'fp-ts/lib/Option'
 import { pipe } from 'fp-ts/lib/function';
 import { server } from 'custom-types'
 import { Message, MessageEmbed, EmbedFieldData } from 'discord.js';
+import { truncate } from '../../utils';
 
 interface NFTListResponse {
     nfts: NFT[];
@@ -37,15 +38,29 @@ export const listNFTs = async (msg: Message, userId: string): Promise<Either<str
     if (nftRes._tag === 'Left') {
         return left(nftRes.left);
     } else {
+        const user = await msg.client.users.fetch(userId);
         const nfts: Array<NFT> = nftRes.right;
         const embedFields: EmbedFieldData[] = nfts.map(nft => ({
             name: 'NFT ID: ' + nft.id,
-            value: nft.fullHash
+            value: 'Type: `' + nft.type._type + '`\n' + viewNftType(nft.type)
         }))
         const embed = new MessageEmbed()
-            .setTitle('Your NFTs')
+            .setAuthor({ name: user.username, iconURL: user.avatarURL() || undefined })
+            .setTitle('NFTs Owned by ' + user.username)
+            .addField(`${nfts.length} Total NFTs`, 'Truncated to the latest 10', false)
             .addFields(embedFields)
         await msg.channel.send({ embeds: [embed] });
         return right(undefined);
+    }
+}
+
+const viewNftType = (nftType: NFTType): string => {
+    if (nftType._type === 'asset') {
+        return 'URL: ' + nftType.url;
+    } else if (nftType._type === 'sticker') {
+        return 'URL: ' + nftType.url;
+    } else {
+        const { truncated, str } = truncate(nftType.content, 64);
+        return (truncated ? '*Text truncated to 64 characters:*\n"' + str + '"' : '"' + str + '"');
     }
 }
