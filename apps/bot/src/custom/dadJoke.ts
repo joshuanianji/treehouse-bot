f3f97faf214366c8377d974fc4cd3b56d9068395
Joshua Ji <joshua.ji@jiwhiz.com>
2022-02-22 22:27:21 -0700

    Funni meme #1231283128731627 Add twitter word listener
    Co-authored-by: LeCitrus <LeCitrus@users.noreply.github.com>

---
+++ apps/bot/src/custom/dadJoke.ts
@@ -0,0 +1,44 @@
+import { Message } from 'discord.js';
+import { truncate } from 'utils';
+
+interface DadJokeReturn { run: boolean, remaining: string };
+
+const triggerWords = ["i'm ", "im ", "i am "];
+
+/**
+ * Checks if the message starts with one of the trigger words
+ * @param word the message content
+ * @returns DadJokeReturn object
+ */
+export const startsWith = (word: string): DadJokeReturn => {
+    for (const triggerWord of triggerWords) {
+        if (word.toLowerCase().startsWith(triggerWord)) {
+            return {
+                run: true,
+                remaining: word.substring(triggerWord.length)
+            }
+        }
+    }
+    return { run: false, remaining: word }
+}
+
+/**
+ * Conditionally runs the dad joke if the message starts with the trigger words.
+ * @param message the Discord message object
+ * @param context The context
+ */
+export const runConditional = async (message: Message<boolean>, context: DadJokeReturn): Promise<void> => {
+    if (context.run) {
+        // only run if Ahmad or Trevor sent it (id 239876252331278347 or 278587415378264064) >:)))
+        if (message.author.id === '239876252331278347' || message.author.id === '278587415378264064') {
+            await message.channel.sendTyping();
+            const { str } = truncate(context.remaining, 1900); // the max is 2000 chars, so we make room for our other msgs
+
+            await message.channel.send({
+                content: `You're not ${str}, you're ${message.author.toString()}!`,
+                files: ['https://c.tenor.com/9RBYPqpnSeUAAAAC/crying-emoji.gif']
+            });
+            console.log(`[INFO]: messageListener - Detected dad joke from ${message.author.username}: id - ${message.id}.`);
+        }
+    }
+}
--- apps/bot/src/custom/passiveMessageListener.ts
+++ apps/bot/src/custom/passiveMessageListener.ts
@@ -1,20 +1,6 @@
 import { Message } from 'discord.js';
-import { truncate } from 'utils';
-
-
-const triggerWords = ["i'm ", "im ", "i am "];
-
-const startsWith = (word: string): { poggers: boolean, remaining: string } => {
-    for (const triggerWord of triggerWords) {
-        if (word.toLowerCase().startsWith(triggerWord)) {
-            return {
-                poggers: true,
-                remaining: word.substring(triggerWord.length)
-            }
-        }
-    }
-    return { poggers: false, remaining: word }
-}
+import * as dadJoke from './dadJoke';
+import * as twitter from './twitter';
 
 const listener = async (message: Message<boolean>): Promise<void> => {
     if (message.author.bot) {
@@ -22,18 +8,12 @@ const listener = async (message: Message<boolean>): Promise<void> => {
     }
 
     try {
-        const { poggers, remaining } = startsWith(message.content);
-        const { str } = truncate(remaining, 1900); // the max is 2000 chars, so we make room for our other msgs
-        if (poggers) {
-            // only run if Ahmad or Trevor sent it (id 239876252331278347 or 278587415378264064) >:)))
-            if (message.author.id === '239876252331278347' || message.author.id === '278587415378264064') {
-                await message.channel.send({
-                    content: `You're not ${str}, you're ${message.author.toString()}!`,
-                    files: ['https://c.tenor.com/9RBYPqpnSeUAAAAC/crying-emoji.gif']
-                });
-                console.log(`[INFO]: messageListener - Detected dad joke from ${message.author.username}: id - ${message.id}.`);
-            }
-        }
+        const dadJokeCtx = dadJoke.startsWith(message.content);
+        const twitterCtx = twitter.startsWith(message.content);
+
+        dadJoke.runConditional(message, dadJokeCtx);
+        twitter.runConditional(message, twitterCtx);
+
     } catch (e) {
         // send message with jerome sus imposter
         await message.channel.send({
