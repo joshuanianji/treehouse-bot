f73fe70c0c97a9042ff723f364e543429445834e
Joshua Ji <joshuanianji@users.noreply.github.com>
2022-04-04 14:13:49 -0600

    Fix Link on `nft` Navbar (#149)
    * Fix link
    
    * Format: use single quotes

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -7,21 +7,21 @@ const Navbar: React.FC = () => {
     const currPage = router.pathname;
 
     const navItems = [
-        { title: "Home", link: "/" },
-        { title: "NFTs", link: "/nfts" },
+        { title: 'Home', link: '/' },
+        { title: 'NFTs', link: '/nft' },
     ];
 
     return (
-        <nav className="bg-gray-800">
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex items-center justify-between h-16">
+        <nav className='bg-gray-800'>
+            <div className='max-w-7xl mx-auto px-2 sm:px-6 lg:px-8'>
+                <div className='relative flex items-center justify-between h-16'>
                     <MainMenuButtons />
-                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex-shrink-0 flex items-center">
-                            <img className="block h-8 w-auto" src="/squat.png" alt="Workflow" />
+                    <div className='flex-1 flex items-center justify-center sm:items-stretch sm:justify-start'>
+                        <div className='flex-shrink-0 flex items-center'>
+                            <img className='block h-8 w-auto' src='/squat.png' alt='Workflow' />
                         </div>
-                        <div className="hidden sm:block sm:ml-6">
-                            <div className="flex space-x-4">
+                        <div className='hidden sm:block sm:ml-6'>
+                            <div className='flex space-x-4'>
                                 {navItems.map(navItem => (
                                     <a
                                         href={navItem.link}
@@ -31,7 +31,7 @@ const Navbar: React.FC = () => {
                                                 : 'text-gray-300 hover:bg-gray-700 hover:text-white'
                                             )}
                                         key={navItem.title}
-                                        aria-current="page">
+                                        aria-current='page'>
                                         {navItem.title}
                                     </a>
                                 ))}
@@ -42,14 +42,14 @@ const Navbar: React.FC = () => {
             </div>
 
             {/* Mobile menu, show/hide based on menu state. */}
-            <div className="sm:hidden" id="mobile-menu">
-                <div className="px-2 pt-2 pb-3 space-y-1">
+            <div className='sm:hidden' id='mobile-menu'>
+                <div className='px-2 pt-2 pb-3 space-y-1'>
                     {navItems.map(navItem => (
                         <a
                             href={navItem.link}
                             key={navItem.title}
-                            className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-                            aria-current="page">{navItem.title}</a
+                            className='bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium'
+                            aria-current='page'>{navItem.title}</a
                         >
                     ))}
                 </div>
@@ -60,42 +60,42 @@ const Navbar: React.FC = () => {
 }
 
 const MainMenuButtons: React.FC = () => (
-    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+    <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
         <button
-            type="button"
-            className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-            aria-controls="mobile-menu"
-            aria-expanded="false"
+            type='button'
+            className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'
+            aria-controls='mobile-menu'
+            aria-expanded='false'
         >
-            <span className="sr-only">Open main menu</span>
+            <span className='sr-only'>Open main menu</span>
             <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
+                className='block h-6 w-6'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                aria-hidden='true'
             >
                 <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M4 6h16M4 12h16M4 18h16'
                 />
             </svg>
             <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
+                className='hidden h-6 w-6'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                aria-hidden='true'
             >
                 <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M6 18L18 6M6 6l12 12'
                 />
             </svg>
         </button>
