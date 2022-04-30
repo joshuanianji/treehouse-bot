030e2bddf913979875327a154f07e6de8368bbb4
Joshua Ji <joshuanianji@users.noreply.github.com>
2022-04-29 23:18:09 -0600

    🔥 Remove unnecessary features (#151)
    Essentially scraps passiveEventListener! Code is still there but it's just doing nothing lol.

--- apps/bot/src/custom/passiveMessageListener.ts
+++ apps/bot/src/custom/passiveMessageListener.ts
@@ -1,4 +1,5 @@
 import { Message } from 'discord.js';
+import { pass } from 'fp-ts/lib/Writer';
 import * as dadJoke from './dadJoke';
 import * as twitter from './twitter';
 
@@ -6,22 +7,7 @@ const listener = async (message: Message<boolean>): Promise<void> => {
     if (message.author.bot) {
         return;
     }
-
-    try {
-        const dadJokeCtx = dadJoke.startsWith(message.content);
-        const twitterCtx = twitter.startsWith(message.content);
-
-        dadJoke.runConditional(message, dadJokeCtx);
-        twitter.runConditional(message, twitterCtx);
-
-    } catch (e) {
-        // send message with jerome sus imposter
-        await message.channel.send({
-            content: `Error in Passive Message Listener! Please contact bot owner lol.`,
-            files: ['https://i.ytimg.com/vi/_GDkeCpT7tA/maxresdefault.jpg']
-        });
-        console.log('[ERROR]: messageListener - Error in passiveMessageListener: ', e);
-    }
+    return;
 }
 
 export default listener;
