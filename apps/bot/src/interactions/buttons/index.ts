45cc2ba65e22880f519456f3b8d8a39f464f0b2c
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-02 23:59:23 -0600

    Delete interactions

--- apps/bot/src/interactions/buttons/index.ts
+++
@@ -1,8 +0,0 @@
-import { buttons as deleteButtons } from "./helpPage/delete";
-import { buttons as helpbackpageButtons } from "./helpPage/helpbackpage";
-import { buttons as helpfirstpageButtons } from "./helpPage/helpfirstpage";
-import { buttons as helpforwardpageButtons } from "./helpPage/helpforwardpage";
-import { buttons as helplastpageButtons } from "./helpPage/helplastpage";
-
-const buttons = [deleteButtons, helpbackpageButtons, helpfirstpageButtons, helpforwardpageButtons, helplastpageButtons];
-export default buttons;
--- apps/bot/src/interactions/slashCommands/index.ts
+++
@@ -1,6 +0,0 @@
-import { slashCommand as roleSlashCommand } from './roles';
-import { slashCommand as rateSlashCommand } from './rate';
-import { slashCommand as saySlashCommand } from './say';
-
-const slashCommands = [roleSlashCommand, rateSlashCommand, saySlashCommand];
-export default slashCommands;
