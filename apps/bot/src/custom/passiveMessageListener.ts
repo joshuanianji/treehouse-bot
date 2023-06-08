6686b4d4dafbb0d6c8bbe0e3545761aed61999f8
Joshua Ji <joshua.ji@jiwhiz.com>
2023-06-08 16:50:33 +0000

    Add back dad joke

--- apps/bot/src/custom/passiveMessageListener.ts
+++ apps/bot/src/custom/passiveMessageListener.ts
@@ -6,7 +6,23 @@ const listener = async (message: Message<boolean>): Promise<void> => {
     if (message.author.bot) {
         return;
     }
-    return;
+
+    try {
+        const dadJokeCtx = dadJoke.startsWith(message.content);
+        // const twitterCtx = twitter.startsWith(message.content);
+
+        dadJoke.runConditional(message, dadJokeCtx);
+        // twitter.runConditional(message, twitterCtx);
+
+    } catch (e) {
+        // send message with jerome sus imposter
+        await message.channel.send({
+            content: `Error in Passive Message Listener! Please contact bot owner lol.`,
+            files: ['https://i.ytimg.com/vi/_GDkeCpT7tA/maxresdefault.jpg']
+        });
+        console.log('[ERROR]: messageListener - Error in passiveMessageListener: ', e);
+    }
 }
+
 
 export default listener;
